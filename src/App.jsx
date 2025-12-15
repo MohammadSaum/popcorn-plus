f6152fbeb200972836b8b07b20e8d3485013f57a
MohammadSaum <mohdsaum14@gmail.com>
2025-12-16 00:09:30 +0530

    landing page 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import logo from './assets/images/logo.png'
+import logo from './assets/images/logo.png';
+import landing from './assets/images/landing page image.png';
+
 const App = () => {
     return (
-        <div className='min-h-screen flex flex-col w-full p-5'>
-            <div className='w-full flex justify-end px-4'>
-                <div className='flex border-[1.5px] rounded-md p-2 cursor-pointer hover:border-[#FFFFFF99] hover:text-[#FFFFFF99] transition duration-150'>LOG IN</div>
-            </div>
-            
-            <div className='h-150 w-190 flex flex-col grow justify-center gap-5 p-2'>
-                <img className='h-30 w-50' src={logo} alt='Disney-Plus-Logo'></img>
-                <span className='text-5xl font-bold w-130'>Stories you'd expect + Stories you wouldn't</span>
+        <div>
+            <div className='min-h-screen flex flex-col w-full p-5'>
+                <div className='w-full flex justify-end px-4'>
+                    <div className='flex border-[1.5px] rounded-md p-2 cursor-pointer hover:border-[#FFFFFF99] hover:text-[#FFFFFF99] transition duration-150'>LOG IN</div>
+                </div>
+                
+                <div className='h-150 w-190 flex flex-col grow justify-center gap-5 p-2'>
+                    <img className='h-30 w-50' src={logo} alt='Disney-Plus-Logo'></img>
+                    <span className='text-5xl font-bold w-130'>Stories you'd expect + Stories you wouldn't</span>
 
-                <div className='flex gap-5 w-full'>
-                    <div className='flex flex-col h-40 w-90 gap-2 p-2'>
-                    <h2 className='text-4xl flex items-baseline gap-1'>$3.99 
-                        <span className='text-lg flex items-center justify-center text-[#FFFFFF99] h-5 border-l-2 pl-1'> Month</span>
-                    </h2>
-                    <span className='text-[#FFFFFF99] text-lg'>Subscription required.</span>
-                    <button className='bg-[#1A86FF] flex p-2 rounded-md justify-center items-center cursor-pointer hover:bg-[#0072C6] active:scale-98 transition duration-150'>SIGN UP NOW</button>
+                    <div className='flex gap-5 w-full'>
+                        <div className='flex flex-col h-40 w-90 gap-2 p-2'>
+                        <h2 className='text-4xl flex items-baseline gap-1'>$3.99 
+                            <span className='text-lg flex items-center justify-center text-[#FFFFFF99] h-5 border-l-2 pl-1'> Month</span>
+                        </h2>
+                        <span className='text-[#FFFFFF99] text-lg'>Subscription required.</span>
+                        <button className='bg-[#1A86FF] flex p-2 rounded-md justify-center items-center cursor-pointer hover:bg-[#0072C6] active:scale-98 transition duration-150'>SIGN UP NOW</button>
+                        </div>
+
+                        <div className='flex flex-col h-40 w-90 gap-2 p-2'>
+                        <h2 className='text-4xl flex items-baseline gap-1'>$39.99 
+                            <span className='text-lg flex items-center justify-center text-[#FFFFFF99] h-5 border-l-2 pl-1'>Year</span>
+                        </h2>
+                        <span className='text-[#FFFFFF99] text-lg'>Save over 15%.* Subscription required.</span>
+                        <button className='bg-[#1A86FF] flex p-2 rounded-md justify-center items-center cursor-pointer hover:bg-[#0072C6] active:scale-98 transition duration-150'>SAVE ON 12 MONTHS</button>
+                        </div>
+                    </div>
+                    <span className='text-[#FFFFFF99] text-xs p-2 -mt-9'>*Saving compared to 12 months of the mostly subscription price.</span>
+
+                    
+                </div>
+            </div>
+
+            <div className='min-h-screen p-15'>
+                <div className='flex gap-10'>
+                    <div className='h-140 w-160'>
+                    <img src={landing} alt='Screen Images'></img>
                     </div>
 
-                    <div className='flex flex-col h-40 w-90 gap-2 p-2'>
-                    <h2 className='text-4xl flex items-baseline gap-1'>$39.99 
-                        <span className='text-lg flex items-center justify-center text-[#FFFFFF99] h-5 border-l-2 pl-1'>Year</span>
-                    </h2>
-                    <span className='text-[#FFFFFF99] text-lg'>Save over 15%.* Subscription required.</span>
-                    <button className='bg-[#1A86FF] flex p-2 rounded-md justify-center items-center cursor-pointer hover:bg-[#0072C6] active:scale-98 transition duration-150'>SAVE ON 12 MONTHS</button>
+                    <div className='pt-13'>
+                        <h2 className='text-5xl font-semibold mb-2'>Watch the way you want</h2>
+                        <div className='px-5 py-4  text-[#FFFFFF99] gap-2 flex flex-col w-130 text-lg'>
+                        <li>
+                            Host virutal movie nights with GroupWatch. Pause, rewind and react with up to six personal friends. To invite or be invited to join GroupWatch, supscription is required
+                        </li>
+                        <li>
+                            Download any movie or series and watch on-the-go
+                        </li>
+                        <li>
+                            Keep your family safe with easy parental controls
+                        </li>
+                        <li>
+                            An ever-growing range of titles in stunning 4K UHD and Dolby Atmos sound on conpatible devices
+                        </li>
+                        <li>
+                            Stream on up to four devices at the same time
+                        </li>
+                        </div>
+                        
                     </div>
                 </div>
-                <span className='text-[#FFFFFF99] text-xs p-2 -mt-9'>*Saving compared to 12 months of the mostly subscription price.</span>
-
-                
+            
+                <div className='w-full mt-3 flex items-center justify-center'>
+                    <button className='bg-[#1A86FF] w-90 flex p-2 rounded-md justify-center items-center cursor-pointer hover:bg-[#0072C6] active:scale-98 transition duration-150'>SIGN UP NOW</button>
+                </div>
             </div>
         </div>
     )

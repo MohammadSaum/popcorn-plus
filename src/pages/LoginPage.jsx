b12b4472fbdd7d5706b3e8c410dc50bc3d6abf9b
MohammadSaum <mohdsaum14@gmail.com>
2026-01-11 00:08:48 +0530

    intial login container setup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const LoginPage = () => {
     return (
-        <div>
-            
+        <div className='min-h-screen min-w-screen p-15 flex items-center justify-around'>
+            <div className='bg-amber-300 h-160 w-140 '></div>
         </div>
     )
 }

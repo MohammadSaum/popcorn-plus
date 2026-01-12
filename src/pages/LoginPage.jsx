5698a17838d69741f3195cbfed58eca211484144
MohammadSaum <mohdsaum14@gmail.com>
2026-01-12 22:34:58 +0530

    Logo for login container

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Logo from '../assets/images/logo.png'
 
 const LoginPage = () => {
     return (
         <div className='min-h-screen min-w-screen p-15 flex items-center justify-around'>
-            <div className='bg-amber-300 h-160 w-140 '></div>
+            <div className='bg-[#0B1022] border border-[#0F1530] rounded-sm h-160 w-140 flex flex-col items-center p-5'>
+                <img src={Logo} className='h-25 w-45'></img>
+
+                <div className='text-[#E6E9F0]'>
+                    Login w email
+                </div>
+            </div>
         </div>
     )
 }

f56f93748d2d9331eaee7a6d40f0f051013bc36e
MohammadSaum <mohdsaum14@gmail.com>
2026-01-18 09:38:10 +0530

    Routes for Signup page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage";
 import LoginPage  from "./pages/LoginPage"
+import SignupPage from "./pages/SignupPage";
 const App = () => {
 
     return (
         <Routes>
             <Route path ='/' element={<LandingPage />} />
             <Route path ='/LoginPage' element={<LoginPage />} />
+            <Route path ='/SignupPage' element={<SignupPage/>} />
         </Routes>
     )
 }

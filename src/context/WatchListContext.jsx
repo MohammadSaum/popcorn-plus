9ef87f7e4a24789c393ebef0b656be73c08828c2
MohammadSaum <mohdsaum14@gmail.com>
2026-01-24 12:22:35 +0530

    wishlist page setup

--- src/context/WatchListContext.jsx
+++ src/context/WatchListContext.jsx
@@ -3,39 +3,39 @@ import { createContext, useContext, useEffect, useState } from "react";
 const WatchlistContext = createContext();
 
 export const WatchlistProvider = ({ children }) => {
-  const [watchlist, setWatchlist] = useState(() => {
-    const stored = localStorage.getItem("watchlist");
-    return stored ? JSON.parse(stored) : [];
-  });
-
-  // persist
-  useEffect(() => {
-    localStorage.setItem("watchlist", JSON.stringify(watchlist));
-  }, [watchlist]);
-
-  const addToWatchlist = (movie) => {
-    setWatchlist((prev) => {
-      const exists = prev.some((m) => m.id === movie.id);
-      if (exists) return prev;
-      return [...prev, movie];
+    const [watchlist, setWatchlist] = useState(() => {
+        const stored = localStorage.getItem("watchlist");
+        return stored ? JSON.parse(stored) : [];
     });
-  };
 
-  const removeFromWatchlist = (id) => {
-    setWatchlist((prev) => prev.filter((m) => m.id !== id));
-  };
+    // persist
+    useEffect(() => {
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    }, [watchlist]);
 
-  const isInWatchlist = (id) => {
-    return watchlist.some((m) => m.id === id);
-  };
+    const addToWatchlist = (movie) => {
+        setWatchlist((prev) => {
+        const exists = prev.some((m) => m.id === movie.id);
+        if (exists) return prev;
+        return [...prev, movie];
+        });
+    };
 
-  return (
-    <WatchlistContext.Provider
-      value={{ watchlist, addToWatchlist, removeFromWatchlist, isInWatchlist }}
-    >
-      {children}
-    </WatchlistContext.Provider>
-  );
+    const removeFromWatchlist = (id) => {
+        setWatchlist((prev) => prev.filter((m) => m.id !== id));
+    };
+
+    const isInWatchlist = (id) => {
+        return watchlist.some((m) => m.id === id);
+    };
+
+    return (
+        <WatchlistContext.Provider
+            value={{ watchlist, addToWatchlist, removeFromWatchlist, isInWatchlist }}
+        >
+            {children}
+        </WatchlistContext.Provider>
+    );
 };
 
 export const useWatchlist = () => useContext(WatchlistContext);

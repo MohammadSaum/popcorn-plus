c10c8773af95629cf6206f63c6aaef8bb307a94c
MohammadSaum <mohdsaum14@gmail.com>
2026-02-16 21:19:30 +0530

    style: enhance MovieRow hover effects

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -6,29 +6,33 @@ const MovieRow = ({title, movies, onMovieClick}) => {
     }))
 
     return (
-        <div className='mt-8 mb-10'>
-            <h2 className='text-xl font-semibold mb-3'>
+        <div className='mt-10 mb-12'>
+            <h2 className='text-xl font-semibold mb-4 tracking-wide'>
                 {title}
             </h2>
 
-            <div className='relative'>
-                <div className='flex gap-4 overflow-x-auto scrollbar-hide pr-10'>
+            <div className='relative group'>
+                <div className="absolute left-0 top-0 h-full w-12 bg-linear-to-r from-app-bg to-transparent z-10 pointer-events-none opacity-0 group-hover:opacity-100  transition-opacity duration-300"/>
+
+                <div className="absolute right-0 top-0 h-full w-12 bg-linear-to-l from-app-bg to-transparent z-10 pointer-events-none opacity-0 group-hover:opacity-100  transition-opacity duration-300"/>
+
+                <div className='flex gap-5 overflow-x-auto scrollbar-hide scroll-smooth snap-x snap-mandatory pb-2'>
                 {moviesWithId.map((movie) => {
                     return (
                         <div 
                             key={movie.id}
-                            className='min-w-37.5 cursor-pointer'
+                            className='min-w-37.5 snap-start cursor-pointer transition-all duration-300 hover:translate-y-2 hover:scale[1.03]'
                             onClick={()=>{onMovieClick(movie)
                             }}
                             >
-                                <div className='w-40 h-60 rounded-lg overflow-hidden bg-gray-800'>
+                                <div className='w-40 h-60 rounded-xl overflow-hidden bg-gray-800 shadow-md hover:shadow-xl transition-shadow duration-300'>
                                     <img 
                                         src={movie.poster} 
                                         alt={movie.title}
-                                        className='w-full h-full object-cover transition-transform duration-300 hover:scale-110'/>
+                                        className='w-full h-full object-cover transition-transform duration-500 hover:scale-110'/>
                                 </div>
 
-                                <p className='text-sm text-center mt-2 text-app-muted'>{movie.title}</p>
+                                <p className='text-sm text-center mt-3 text-app-muted line-clamp-2'>{movie.title}</p>
                         </div>
                     )
                 })}
